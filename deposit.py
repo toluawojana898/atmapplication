8274309276cbec4818d879a87eb2cb03e008b14f
toluawojana898 <65146664+toluawojana898@users.noreply.github.com>
2020-05-12 23:27:16 -0500

    Update deposit.py

--- deposit.py
+++ deposit.py
@@ -5,7 +5,7 @@ def cashAdd(username,password):
 	account_number, balance, __, __ = qdb.query_db(qdb.customer_info_query,username,password)[0]
 	amount = int(input("Enter the amount you want to deposit: "))
 	final_amount = int(balance) + amount
-	qdb.query_db(qdb.transfer_to_query,final_amount,account_number)
+	qdb.query_db(qdb.transfer_to_query,amount,account_number)
 	#We have to check this
 	print('The deposit of {} was successful for account number {} you now have a balance of {}'.format(amount,account_number,final_amount))
 	return True
@@ -15,7 +15,7 @@ def cashRemove(username,password):
 	amount = int(input("Enter the amount you would like to withdraw: "))
 	if amount <= int(balance):
 		final_amount = int(balance) - amount
-		qdb.query_db(qdb.transfer_from_query,final_amount,account_number)
+		qdb.query_db(qdb.transfer_from_query,amount,account_number)
 		print('Withdrawal successful!, your current balance is {}'.format(final_amount))
 		return True
 	else:
@@ -27,14 +27,14 @@ def transfercash(username,password):
 	receiver_acc_num = int(input('Recipient\'s Account Number: '))
 	amount_send = int(input('Amount you want to transfer: '))
 	account_number = qdb.query_db(qdb.account_query)
-	__, balance, __, __ = qdb.query_db(qdb.customer_info_query,username,password)[0]
+	your_acc_num, balance, __, __ = qdb.query_db(qdb.customer_info_query,username,password)[0]
 	if receiver_acc_num in account_number:
 		print('This account belongs to {}'.format(receiver_name))
 		print(account_number)
 		if amount_send <= int(balance):
 			final_amount = int(balance) - amount_send
-			qdb.query_db(qdb.transfer_from_query,amount_send,receiver_acc_num)
-			#qdb.query_db(qdb.transfer_to_query,amount_send,account_number)
+			qdb.query_db(qdb.transfer_from_query,amount_send,your_acc_num)
+			qdb.query_db(qdb.transfer_to_query,amount_send,receiver_acc_num)
 			print('You have transferred {} to {}'.format(amount_send,receiver_name))
 		else:
 			print('There is no sufficient funds to complete this operation')
