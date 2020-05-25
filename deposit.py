4f6b2bdc6471c583bdaaaf18f374ee6721891f50
toluawojana898 <65146664+toluawojana898@users.noreply.github.com>
2020-05-24 22:35:23 -0500

    Update deposit.py

--- deposit.py
+++ deposit.py
@@ -1,45 +1,90 @@
 import query_db as qdb
 import login
+import getpass as pas 
+from datetime import date
 
 def cashAdd(username,password):
-	account_number, balance, __, __ = qdb.query_db(qdb.customer_info_query,username,password)[0]
+	account_number, balance, ___ = qdb.query_db(qdb.display_customer_data_query,username,password)[0]
 	amount = int(input("Enter the amount you want to deposit: "))
 	final_amount = int(balance) + amount
 	qdb.query_db(qdb.transfer_to_query,amount,account_number)
 	#We have to check this
+	#update the log
 	print('The deposit of {} was successful for account number {} you now have a balance of {}'.format(amount,account_number,final_amount))
-	return True
+	qdb.logger.debug('Deposit of {} successful for {}'.format(amount,account_number))
 
 def cashRemove(username,password):
-	account_number, balance, __, __ = qdb.query_db(qdb.customer_info_query,username,password)[0]
+	account_number, balance, ___ = qdb.query_db(qdb.display_customer_data_query,username,password)[0]
 	amount = int(input("Enter the amount you would like to withdraw: "))
 	if amount <= int(balance):
 		final_amount = int(balance) - amount
 		qdb.query_db(qdb.transfer_from_query,amount,account_number)
 		print('Withdrawal successful please take your cash!, your current balance is {}'.format(final_amount))
+		qdb.logger.debug('Withdrawal of {} successful for {}'.format(amount,account_number))
 		return True
 	else:
 		print('Insufficient Funds!')
-	return False
+		qdb.logger.debug('Insufficient funds for Account number: {}'.format(account_number))
+		return False
 
-def transfercash(username,password):
-	receiver_name = input('Recipients full name: ')
-	receiver_acc_num = int(input('Recipient\'s Account Number: '))
-	amount_send = int(input('Amount you want to transfer: '))
-	account_number = qdb.query_db(qdb.account_query)
-	your_acc_num, balance, __, __ = qdb.query_db(qdb.customer_info_query,username,password)[0]
-	if receiver_acc_num in account_number:
-		print('This account belongs to {}'.format(receiver_name))
-		print(account_number)
-		if amount_send <= int(balance):
-			final_amount = int(balance) - amount_send
-			qdb.query_db(qdb.transfer_from_query,amount_send,your_acc_num)
-			qdb.query_db(qdb.transfer_to_query,amount_send,receiver_acc_num)
-			print('You have transferred {} to {}'.format(amount_send,receiver_name))
-		else:
-			print('There is no sufficient funds to complete this operation')
-	else:
-		print('Recipient does not exist')
+def transfercash(account_number):
+	try:
+		receiver_name = input('Recipients full name: ')
+		receiver_acc_num = int(input('Recipient\'s Account Number: '))
+		amount_send = int(input('Amount you want to transfer: '))
+		rcv_acc_num = qdb.query_db(qdb.check_account_query,receiver_acc_num)[0]
+		if receiver_acc_num not in rcv_acc_num:
+			print('Recipient does not exist')
+	except:
+		print('Something went wrong :)')
+		return False	
+
+	try:
+		account_number, balance = qdb.query_db(qdb.customer_transfer_query,account_number, amount_send)[0]
+		print('You have transferred {} to {}'.format(amount_send,receiver_name))
+		qdb.logger.debug('Successfully debited account number {} with {}'.format(account_number,amount_send))
+		qdb.logger.debug('Successfully credited account number {} with {}'.format(receiver_acc_num,amount_send))
+	except:
+		print('Insufficient Funds to complete this operation')
+		qdb.logger.debug('Insufficient funds for Account Number {}'.format(account_number))
+		#print('This account belongs to {}'.format(receiver_name))
+		#print(account_number)
+	qdb.query_db(qdb.transfer_from_query,amount_send,account_number)
+	qdb.query_db(qdb.transfer_to_query,amount_send,receiver_acc_num)
+
+#else:
+#print('There is no sufficient funds to complete this operation')
+#if amount_send >= int(balance):
+			#final_amount = int(balance) - amount_send
+def accNew():
+	
+	#check_customer_username = ("SELECT username FROM atm.customer;")
+	fullname = input('Enter your fullname: ').title()
+	newuser = input('Enter a unique login username: ')
+	user_name = qdb.query_db(qdb.check_customer_username,newuser)[0]
+	while newuser in user_name:
+		print('Not available! use another name')
+		qdb.logger.debug('Username: {} already exists'.format(newuser))
+		newuser = input('Enter a unique login username again: ')
+	pd = pas.getpass(prompt='Enter a login password:')
+	gender = input('Gender! Enter either "M" or "F" only: ').upper()
+	email = input('Enter a valid email address: ')
+	dob = input('Enter dte of birth in this format YYYY MM DD: ')
+	dob = dob.split(' ')
+	dob[0] = int(dob[0])
+	dob[1] = int(dob[1])
+	dob[2] = int(dob[2])
+	#(dob[0],dob[1],dob[2])
+	qdb.query_db(qdb.insert_cusomer_data_query,fullname, newuser, pd, gender, email, date(dob[0],dob[1],dob[2]))
+	balance = int(input('Enter your opening balance: '))
+	account_type = input('Enter account type,"Savings" or "Current": ').title()
+	account_number = qdb.query_db(qdb.get_account_query,newuser, pd)[0][0]
+	qdb.query_db(qdb.insert_account_data_query, account_number, balance, account_type)
+	qdb.logger.debug('Account number {} successfully created for {}'.format(account_number,fullname))
+	print('Welcome to TEEJAY\'s BANK'.center(60))
 	
 	#Update the database with the value transferred to the beneficiary
 	#Send a mail notifying the recipient of the successful transfer
+		
+	
+
