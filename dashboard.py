acc1e6644b7c5a91b02346e786702a52a30b8468
toluawojana898 <65146664+toluawojana898@users.noreply.github.com>
2020-05-24 22:34:09 -0500

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -2,24 +2,25 @@ import login
 import query_db as query
 
 def dashboard(username,password):
-	account_number, balance, fullname, email = query.query_db(query.customer_info_query,username,password)[0]
+	account_number, balance, fullname = query.query_db(query.display_customer_data_query,username,password)[0]
 	print('_'*85)
 	print('|','Welcome to Teejay\'s Bank'.center(82),'|')
 	print('|','_'*82,'|')
 	print('|','You\'re in!!!' .center(82),'|')	
 	print('|', ' '*82, '|')
 	print('|', ' '*82, '|')
-	print('|', '\tNAME:  {}'.format(fullname),' '*52,'|')
+	print('|', '\tNAME:  {}'.format(fullname),' '*56,'|')
 	print('|',' '*82, '|')
 	print('|',' '*82, '|')
-	print('|','\tAccount Number\t{}'.format(account_number),'\t' 'Account Balance:\t{}'.format(balance),' '*22,'|')
+	print('|','\tAccount Number\t{}'.format(account_number),'\t' 'Account Balance:\t{}'.format(balance),' '*20,'|')
 	print('|','-'*82,'|')
 	print('|',' '*82, '|')
 	print('|','\tHow can we help you today?',' '*49, '|')
 	print('|','-'*82,'|')
 	print('|',' '*82,'|')
-	print('|','\tCheck Balance:     1', '\t\t\t', '\tTransfer Money:    2',' '*7,'|')
+	print('|','\tCash Deposit:     1', '\t\t\t', '\tCash Withdrawal:    2',' '*6,'|')
 	print('|',' '*82,'|')
-	print('|','\tCash Withdrawal:   3', '\t\t\t', '\tOpen New Account:  4',' '*7,'|')
+	print('|','\tTransfer:         3', '\t\t\t', '\tExit:               4',' '*6,'|')
 	print('|',' '*82,'|')
 	print('-'*85)
+	return (account_number, balance)
